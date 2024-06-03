a758b9f98a727315a0db78dced331ccfd831a1b6
rishimehta24 <122311633+rishimehta24@users.noreply.github.com>
2024-06-03 18:38:24 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update and upgrade the system
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install Python pip
+sudo apt-get install -y python3-pip
+
+# Install ffmpeg
+sudo apt-get install -y ffmpeg
+
+# Install paplay
+sudo apt-get install -y pulseaudio
+
+# Install the required Python packages
+pip3 install -r requirements.txt
